28dc723e20b9fb6dd75013ba0b52173d85646054
Mattia46 <mattia.assogna@gmail.com>
2015-12-02 17:18:05 +0000

    Added the second protractor test and passed

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,8 +1,11 @@
-describe('Github Profile finder', function() {
-  it('has a title', function() {
+describe('GitHub profile finder', function() {
+  it('finds profiles', function() {
     browser.get('http://localhost:8080');
 
-    expect(browser.getTitle()).toEqual('Github User Search');
+    element(by.model('searchCtrl.searchTerm')).sendKeys('Mattia46');
+    element(by.className('btn')).click();
+
+    expect(element(by.binding('item.login')).getText()).
+    toEqual('Mattia46');
   });
 });
-
