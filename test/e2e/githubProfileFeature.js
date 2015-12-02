3d29e123b5bd80616227901edebfc92a4d561b38
Mattia46 <mattia.assogna@gmail.com>
2015-12-02 19:01:04 +0000

    added the element.all test and user.each

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,11 +1,47 @@
-describe('GitHub profile finder', function() {
-  it('finds profiles', function() {
-    browser.get('http://localhost:8080');
+//describe('GitHub profile finder', function() {
+  //it('finds profiles', function() {
+    //browser.get('http://localhost:8080');
 
-    element(by.model('searchCtrl.searchTerm')).sendKeys('Mattia46');
-    element(by.className('btn')).click();
+    //element(by.model('searchCtrl.searchTerm')).sendKeys('Mattia46');
+    //element(by.className('btn')).click();
 
-    expect(element(by.binding('item.login')).getText()).
-    toEqual('Mattia46');
-  });
-});
+    //expect(element(by.binding('item.login')).getText()).
+    //toEqual('Mattia46');
+    //});
+    //});
+
+    describe('GitHub profile finder', function() {
+
+      var searchBox = element(by.model('searchCtrl.searchTerm'));
+      var searchButton = element(by.className('btn'));
+
+      beforeEach(function() {
+        browser.get('http://localhost:8080');
+      });
+
+      it('has a title', function() {
+        expect(browser.getTitle()).toEqual('Github User Search');
+      });
+
+      it('finds profiles', function() {
+        searchBox.sendKeys('Mattia46');
+        searchButton.click();
+        expect(element(by.binding('item.login')).getText()).toEqual('Mattia46');
+      });
+
+      it('finds profiles', function() {
+
+        searchBox.sendKeys('Mattia46');
+        searchButton.click();
+
+        var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+        profiles.each(function(user) {
+          user.getText().then(function(text) {
+            expect(text()).toContain('Mattia46');
+          });
+        });
+        ////expect(element(by.binding('item.lenght')).getText()).toBe(1);
+        //expect(profiles.last().getText()).toEqual(['Mattia46']); //This won't pass!
+      });
+
+    });
