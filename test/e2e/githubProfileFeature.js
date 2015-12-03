5252a5e3cbb228854d036b89b1e2318450051348
Mattia46 <mattia.assogna@gmail.com>
2015-12-03 10:18:31 +0000

    completed the second walkthrough and resized the github pictures

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -7,41 +7,42 @@
 
     //expect(element(by.binding('item.login')).getText()).
     //toEqual('Mattia46');
-    //});
-    //});
+  //});
+//});
 
-    describe('GitHub profile finder', function() {
+describe('GitHub profile finder', function() {
 
-      var searchBox = element(by.model('searchCtrl.searchTerm'));
-      var searchButton = element(by.className('btn'));
+  var searchBox = element(by.model('searchCtrl.searchTerm'));
+  var searchButton = element(by.className('btn'));
 
-      beforeEach(function() {
-        browser.get('http://localhost:8080');
+  beforeEach(function() {
+    browser.get('http://localhost:8080');
+  });
+
+  it('has a title', function() {
+    expect(browser.getTitle()).toEqual('Github User Search');
+  });
+
+  it('finds profiles', function() {
+    searchBox.sendKeys('Mattia46');
+    searchButton.click();
+    //browser.pause();
+    expect(element(by.binding('item.login')).getText()).toEqual('Mattia46');
+  });
+
+  it('finds profiles', function() {
+
+    searchBox.sendKeys('Mattia46');
+    searchButton.click();
+
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    profiles.each(function(user) {
+      user.getText().then(function(text) {
+        expect(text()).toContain('Mattia46');
       });
-
-      it('has a title', function() {
-        expect(browser.getTitle()).toEqual('Github User Search');
-      });
-
-      it('finds profiles', function() {
-        searchBox.sendKeys('Mattia46');
-        searchButton.click();
-        expect(element(by.binding('item.login')).getText()).toEqual('Mattia46');
-      });
-
-      it('finds profiles', function() {
-
-        searchBox.sendKeys('Mattia46');
-        searchButton.click();
-
-        var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
-        profiles.each(function(user) {
-          user.getText().then(function(text) {
-            expect(text()).toContain('Mattia46');
-          });
-        });
-        ////expect(element(by.binding('item.lenght')).getText()).toBe(1);
-        //expect(profiles.last().getText()).toEqual(['Mattia46']); //This won't pass!
-      });
-
     });
+    ////expect(element(by.binding('item.lenght')).getText()).toBe(1);
+    //expect(profiles.last().getText()).toEqual(['Mattia46']); //This won't pass!
+  });
+
+});
